cd91827a466eefde963bf74f1cd16e97a58f19ef
Cesare Domizi <domizicesare@gmail.com>
2023-09-11 16:27:29 +0200

    refactor appbar links

--- src/layouts/TopBar/MenuItem.tsx
+++ src/layouts/TopBar/MenuItem.tsx
@@ -7,24 +7,20 @@ interface MenuItemProps {
   url: string;
 }
 
-const MenuItem = ({ title, url }: MenuItemProps) => {
-  const CustomNavLink = () => (
-    <NavLink
-      to={url}
-      style={({ isActive }) => ({
-        color: "inherit",
-        textDecoration: "none",
-        ...(isActive && {
-          borderBottom: "3px solid white",
-          borderRadius: 0,
-        }),
-      })}
-    >
-      <Button sx={{ color: "inherit" }}>{title.toUpperCase()}</Button>
-    </NavLink>
-  );
-
-  return <CustomNavLink />;
-};
+const MenuItem = ({ title, url }: MenuItemProps) => (
+  <NavLink
+    to={url}
+    style={({ isActive }) => ({
+      color: "inherit",
+      textDecoration: "none",
+      ...(isActive && {
+        borderBottom: "3px solid white",
+        borderRadius: 0,
+      }),
+    })}
+  >
+    <Button sx={{ color: "inherit" }}>{title.toUpperCase()}</Button>
+  </NavLink>
+);
 
 export default MenuItem;
