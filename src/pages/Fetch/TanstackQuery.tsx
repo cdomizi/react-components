f54c86332261f7d3dda206150b137b937a7dc09b
cdomizi <domizicesare@gmail.com>
2023-10-07 13:29:41 +0200

    add getProduct to TanstackQuery

--- src/pages/Fetch/TanstackQuery.tsx
+++ src/pages/Fetch/TanstackQuery.tsx
@@ -1,21 +1,71 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+
+import { delayAxiosRequest } from "../../utils/delay";
 import Logger from "../../components/Logger";
 
 import { Box, Button, Typography } from "@mui/material";
 
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  brand: string;
+};
+
+type ProductQuery = {
+  method?: "GET" | "POST";
+  data: Product;
+};
+
 const TanstackQuery = () => {
-  const result = useQuery({
+  const [isProductFetched, setIsProductFetched] = useState(false);
+  const getProduct = async (): Promise<ProductQuery> =>
+    // Artificially delay function to show loading state
+    delayAxiosRequest(await axios.get("https://dummyjson.com/product/1"));
+
+  const productQuery = useQuery<ProductQuery, AxiosError<Product>>({
     queryKey: ["product"],
-    queryFn: () => axios.get("https://dummyjson.com/product/1"),
+    queryFn: getProduct,
+    enabled: isProductFetched,
   });
+
+  const result = (() => {
+    if (!isProductFetched) return null;
+
+    switch (productQuery.status) {
+      case "loading":
+        return "loading...";
+      case "error": {
+        const { code, response, message } = productQuery.error;
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        return `${code} ${response?.status || 500}: ${message}`;
+      }
+      case "success": {
+        const {
+          data: { id, title, brand, price },
+        } = productQuery.data;
+        return {
+          method: "GET",
+          data: { id, title, brand, price },
+        };
+      }
+      default:
+        return null;
+    }
+  })();
 
   return (
     <Box>
       <Typography variant="h4" paragraph>
         Tanstack Query
       </Typography>
-      <Button onClick={() => null} variant="outlined" size="small">
+      <Button
+        onClick={() => setIsProductFetched(true)}
+        variant="outlined"
+        size="small"
+      >
         Get product
       </Button>
       <Button onClick={() => null} variant="outlined" size="small">
