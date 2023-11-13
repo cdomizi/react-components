53df31b905514ba9c7bdaf870f96bd0a9fcc6f3f
cdomizi <domizicesare@gmail.com>
2023-11-13 20:26:39 +0100

    add optional timeout argument to delay functions

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -3,16 +3,17 @@ const delayFunc = async (func: () => unknown, ms?: number) =>
     setTimeout(() => resolve(func()), ms ?? 2000);
   });
 
-const delayRequest = async <TData>(value: TData) =>
+const delayRequest = async <TData>(value: TData, ms?: number) =>
   new Promise<TData>((resolve) => {
-    setTimeout(() => resolve(value), 2000);
+    setTimeout(() => resolve(value), ms ?? 2000);
   });
 
 const delayAxiosRequest = async <TData>(
   value: TData | PromiseLike<TData>,
+  ms?: number,
 ): Promise<TData> =>
   new Promise((resolve) => {
-    setTimeout(() => resolve(value), 2000);
+    setTimeout(() => resolve(value), ms ?? 2000);
   });
 
 export { delayFunc, delayRequest, delayAxiosRequest };
