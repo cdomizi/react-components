223be899b0d27123d1e01ca4c3a632d39d0c884e
cdomizi <domizicesare@gmail.com>
2023-11-13 17:09:40 +0100

    add tests for function getRandomData

--- src/utils/getRandomData.test.ts
+++ src/utils/getRandomData.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, test } from "vitest";
-import { getRandomInt } from "./getRandomData";
+import { UserType } from "../types";
+import { getRandomInt, getRandomData } from "./getRandomData";
 
-describe("getRandomData", () => {
+describe("getRandomInt", () => {
   test("return random integer between 1 and 10 with no arguments", () => {
     expect(getRandomInt()).toBeGreaterThanOrEqual(1);
     expect(getRandomInt()).toBeLessThanOrEqual(10);
@@ -15,5 +16,29 @@ describe("getRandomData", () => {
   test("return random integer between provided arguments", () => {
     expect(getRandomInt(12, 5)).toBeGreaterThanOrEqual(5);
     expect(getRandomInt(12, 5)).toBeLessThanOrEqual(12);
+  });
+});
+
+describe("getRandomData", () => {
+  test("returns object with expected properties", async () => {
+    const randomUser = await getRandomData<UserType>(
+      "https://dummyjson.com/users/",
+    );
+    expect(randomUser).toHaveProperty("username");
+    expect(randomUser).toHaveProperty("email");
+  });
+
+  test("returns expected data if provided with second argument", async () => {
+    const getUser = async () => {
+      const response = await fetch("https://dummyjson.com/users/1");
+      const data = (await response.json()) as Promise<UserType>;
+      return data;
+    };
+    const expectedUser = await getUser();
+    const user = await getRandomData<UserType>(
+      "https://dummyjson.com/users/",
+      1,
+    );
+    expect(user).toEqual(expectedUser);
   });
 });
