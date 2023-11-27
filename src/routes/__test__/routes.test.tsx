6edb741a3194d39885dc5b674991907f023abce0
cdomizi <domizicesare@gmail.com>
2023-11-27 21:17:04 +0100

    add routing tests with user interaction

--- src/routes/__test__/routes.test.tsx
+++ src/routes/__test__/routes.test.tsx
@@ -1,15 +1,27 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Outlet, Route, Routes } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { Link, MemoryRouter, Outlet, Route, Routes } from "react-router-dom";
 
 // Project import
 import RootLayout from "../../layouts/RootLayout";
+import TopBar from "../../layouts/TopBar";
 import Home from "../../pages/Home";
 import ErrorPage from "../../pages/ErrorPage";
 import Todos from "../../pages/Todos";
 
+vi.mock("../../layouts/TopBar", () => ({
+  default: () => (
+    <header>
+      <Link to="/">myApp</Link>
+      <Link to="/todos">Todos</Link>
+    </header>
+  ),
+}));
+
 vi.mock("../../layouts/RootLayout", () => ({
   default: () => (
     <div>
+      <TopBar />
       <Outlet />
     </div>
   ),
@@ -49,7 +61,7 @@ const TestRoutes = () => (
 );
 
 describe("router", () => {
-  test("renders home page on default route", () => {
+  test("render home page on default route", () => {
     const MainRoutes = vi.fn().mockImplementation(TestRoutes);
 
     render(
@@ -62,7 +74,7 @@ describe("router", () => {
     expect(pageTitle).toMatch(/home/i);
   });
 
-  test("renders page component for specific route", () => {
+  test("render page component for specific route", () => {
     const MainRoutes = vi.fn().mockImplementation(TestRoutes);
 
     render(
@@ -72,6 +84,40 @@ describe("router", () => {
     );
     const pageTitle = screen.getByRole("heading");
 
+    expect(pageTitle).toMatch(/todo list/i);
+  });
+
+  test("navigation from the top bar's home button", async () => {
+    const MainRoutes = vi.fn().mockImplementation(TestRoutes);
+
+    render(
+      <MemoryRouter initialEntries={["/todos"]}>
+        <MainRoutes />
+      </MemoryRouter>,
+    );
+    const user = userEvent.setup();
+    const homeButton = screen.getByText(/myapp/i);
+
+    await user.click(homeButton);
+
+    const pageTitle = screen.getByRole("heading");
+    expect(pageTitle).toMatch(/home/i);
+  });
+
+  test("navigation from the topbar links", async () => {
+    const MainRoutes = vi.fn().mockImplementation(TestRoutes);
+
+    render(
+      <MemoryRouter>
+        <MainRoutes />
+      </MemoryRouter>,
+    );
+    const user = userEvent.setup();
+    const todosNavLink = screen.getByRole("link", { name: "Todos" });
+
+    await user.click(todosNavLink);
+
+    const pageTitle = screen.getByRole("heading");
     expect(pageTitle).toMatch(/todo list/i);
   });
 });
