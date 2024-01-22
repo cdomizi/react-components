b81d5bc347fb93d0f805159683f802164a060827
cdomizi <domizicesare@gmail.com>
2024-01-22 23:51:12 +0100

    add remaining tests for TanstackQuery component

--- src/pages/Fetch/__test__/TanstackQuery.test.tsx
+++ src/pages/Fetch/__test__/TanstackQuery.test.tsx
@@ -1,8 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import axios, { AxiosResponse } from "axios";
+import { allProducts, newProduct } from "mocks/data";
 import { ReactNode } from "react";
+import { notFoundError } from "utils/__test__/axiosErrorHandler.test";
 import { TanstackQuery } from "../TanstackQuery";
 
+// Set up query client for testing
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,6 +18,11 @@ const queryClient = new QueryClient({
 const wrapper = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
+
+// Clear query client after each test
+afterEach(() => {
+  queryClient.clear();
+});
 
 describe("TanstackQuery", () => {
   test("component renders correctly", () => {
@@ -35,9 +45,98 @@ describe("TanstackQuery", () => {
     expect(addProductErrorButton).toBeInTheDocument();
   });
 
-  test.todo("get product");
+  test("get product", async () => {
+    const productId = 1;
+    const expectedProduct = allProducts.find(
+      (product) => product.id === productId,
+    );
 
-  test.todo("add product");
+    const user = userEvent.setup();
 
-  test.todo("add product error");
+    const axiosGetSpy = vi
+      .spyOn(axios, "get")
+      .mockResolvedValueOnce({ data: expectedProduct });
+
+    render(<TanstackQuery />, { wrapper });
+
+    const getProductButton = screen.getByRole("button", {
+      name: /^get product$/i,
+    });
+
+    // Get product data
+    await user.click(getProductButton);
+
+    const loadingText = screen.getByText(/loading/i);
+
+    // Loading text being displayed
+    expect(loadingText).toBeInTheDocument();
+
+    const responseData = (await axiosGetSpy.mock.results[0]
+      .value) as AxiosResponse;
+
+    // Response data fetched correctly
+    expect(axiosGetSpy).toHaveBeenCalledOnce();
+    expect(axiosGetSpy).toHaveBeenCalledWith(
+      `https://dummyjson.com/product/${productId}`,
+    );
+    expect(await responseData.data).toStrictEqual(expectedProduct);
+  });
+
+  test("add product", async () => {
+    const user = userEvent.setup();
+
+    const axiosPostSpy = vi
+      .spyOn(axios, "post")
+      .mockResolvedValueOnce({ data: newProduct });
+
+    render(<TanstackQuery />, { wrapper });
+
+    const addProductButton = screen.getByRole("button", {
+      name: /^add product$/i,
+    });
+
+    // Create new product
+    await user.click(addProductButton);
+
+    const loadingText = screen.getByText(/loading/i);
+
+    // Loading text being displayed
+    expect(loadingText).toBeInTheDocument();
+
+    const responseData = (await axiosPostSpy.mock.results[0]
+      .value) as AxiosResponse;
+
+    // New product created correctly
+    expect(axiosPostSpy).toHaveBeenCalledOnce();
+    expect(await responseData.data).toStrictEqual(newProduct);
+  });
+
+  test("add product error", async () => {
+    const user = userEvent.setup();
+
+    const axiosPostSpy = vi
+      .spyOn(axios, "post")
+      .mockResolvedValueOnce(notFoundError);
+
+    render(<TanstackQuery />, { wrapper });
+
+    const addErrorButton = screen.getByRole("button", {
+      name: /^add product error$/i,
+    });
+
+    // Trigger error while getting product data
+    await user.click(addErrorButton);
+
+    const loadingText = screen.getByText(/loading/i);
+
+    // Loading text being displayed
+    expect(loadingText).toBeInTheDocument();
+
+    const responseData = (await axiosPostSpy.mock.results[0]
+      .value) as AxiosResponse;
+
+    // Get 404 response
+    expect(axiosPostSpy).toHaveBeenCalledOnce();
+    expect(responseData).toStrictEqual(notFoundError);
+  });
 });
