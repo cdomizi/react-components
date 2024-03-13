f3cde8a8f0ebf52941646b68c6528bd0c17ab5ce
cdomizi <domizicesare@gmail.com>
2024-03-13 21:09:00 +0100

    add tests for useFetch custom hook

--- src/hooks/__test__/useFetch.test.ts
+++ src/hooks/__test__/useFetch.test.ts
@@ -1,5 +1,6 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { useFetch } from "hooks/useFetch";
+import { allUsers } from "mocks/data";
 
 const API_ENDPOINT = import.meta.env.VITE_REACT_APP_BASE_API_URL;
 
@@ -14,15 +15,48 @@ describe("useFetch", () => {
   });
 
   test("loading state", async () => {
-    const { loading, error, data } = renderHook(() => useFetch(API_ENDPOINT))
-      .result.current;
+    const { result } = renderHook(() => useFetch(API_ENDPOINT));
 
-    expect(loading).toBe(true);
-    expect(error).toBeUndefined();
-    expect(data).toBeUndefined();
+    expect(result.current.loading).toBe(true);
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.data).toBeUndefined();
 
     await waitFor(() => {
-      expect(loading).toBe(false);
+      expect(result.current.loading).toBe(false);
+    });
+  });
+
+  test("error on failed fetching data", async () => {
+    const NOT_FOUND_ENDPOINT = "https://dummyjson.com/NOT_FOUND";
+
+    const { result } = renderHook(() => useFetch(NOT_FOUND_ENDPOINT));
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.data).toBeUndefined();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.error).toBeDefined();
+      expect(result.current.error?.message).toBe("Not Found");
+    });
+  });
+
+  test("data fetched and stored in the cache", async () => {
+    const userId = "1";
+    const USER_ENDPOINT = `https://dummyjson.com/users/${userId}`;
+    const expectedUser = allUsers.find((user) => user.id.toString() === userId);
+
+    const { result } = renderHook(() => useFetch(USER_ENDPOINT));
+
+    expect(result.current.loading).toBe(true);
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.data).toBeUndefined();
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.error).toBeUndefined();
+      expect(result.current.data).toStrictEqual(expectedUser);
     });
   });
 });
