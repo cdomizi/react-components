1a9e635b09823ab41ec3cd7aa2137701ed4c40eb
cdomizi <domizicesare@gmail.com>
2024-03-17 18:15:35 +0100

    refactor tests for getRandomData utility

--- src/utils/__test__/getRandomData.test.ts
+++ src/utils/__test__/getRandomData.test.ts
@@ -3,25 +3,26 @@ import { UserType } from "types";
 import { getRandomData } from "utils/getRandomData";
 import * as getRandomInt from "utils/getRandomInt";
 
-describe("getRandomData", () => {
-  const expectedRandomUser = allUsers[0];
+const userId = 1;
+const expectedUser = allUsers.find((user) => user.id === userId);
 
+describe("getRandomData", () => {
   test("returns object with expected properties", async () => {
-    vi.spyOn(getRandomInt, "getRandomInt").mockReturnValue(1);
+    vi.spyOn(getRandomInt, "getRandomInt").mockReturnValue(userId);
 
     const randomUser = await getRandomData<UserType>(
       "https://dummyjson.com/users",
     );
 
-    expect(randomUser).toStrictEqual(expectedRandomUser);
+    expect(randomUser).toStrictEqual(expectedUser);
   });
 
   test("returns expected data if provided with second argument", async () => {
     const user = await getRandomData<UserType>(
       "https://dummyjson.com/users",
-      expectedRandomUser.id,
+      userId,
     );
 
-    expect(user).toStrictEqual(expectedRandomUser);
+    expect(user).toStrictEqual(expectedUser);
   });
 });
--- src/utils/getRandomData.ts
+++ src/utils/getRandomData.ts
@@ -4,5 +4,6 @@ export const getRandomData = async <T>(url: string, id?: number) => {
   const randomId = id ?? getRandomInt(30);
   const response = await fetch(`${url}/${randomId}`);
   const data = (await response.json()) as T;
+
   return data;
 };
